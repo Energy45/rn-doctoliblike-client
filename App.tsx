1215edd40d2458ee20630c4e5361f1cb233c98b2
Dylan ANTONIOTTI <dylan.antoniotti@gmail.com>
2022-12-13 17:19:23 +0100

    feat: add register form

--- App.tsx
+++ App.tsx
@@ -1,12 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
+import { NativeBaseProvider } from 'native-base';
 import { StyleSheet, Text, View } from 'react-native';
+import RegisterForm from './components/RegisterForm';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>AH</Text>
-      <StatusBar style="auto" />
-    </View>
+    // <View style={styles.container}>
+    //   <Text>AH</Text>
+    //   <StatusBar style="auto" />
+    // </View>
+    <NativeBaseProvider>
+      <RegisterForm/>
+    </NativeBaseProvider>
   );
 }
 
--- components/RegisterForm.tsx
+++ components/RegisterForm.tsx
@@ -1,5 +1,35 @@
-const RegisterForm = () => {
+import { Button, Input, Flex, Spacer } from "native-base";
+import { View, StyleSheet, Text } from "react-native";
 
+const RegisterForm = () => {
+    return (
+        <Flex align='center' justify='center'>
+            <Text>Register</Text>
+            <Spacer/>
+            <Input
+                placeholder="Email"
+                type="text"
+                mx={10}
+                my={20}
+            />
+             <Spacer/>
+            <Input
+                placeholder="Password"
+                type="password"
+            />
+            <Button>Register</Button>
+        </Flex>
+        // <View style={styles.wrapper}>
+        // </View>
+    )
 }
+
+const styles = StyleSheet.create({
+    wrapper: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+})
 
 export default RegisterForm;
